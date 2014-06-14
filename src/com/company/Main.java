6c870ff7b3224bba6600d3a1ff4ba9816f0b6e98
GSerjo <sedot8@gmail.com>
2014-06-15 00:30:50 +0400

    - .idea

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World example");
         System.out.println("Input your name:");
-
         Scanner scanner = new Scanner(System.in);
+
         String value = scanner.next();
-
         System.out.println(String.format("Hello %s", value));
-
     }
 }
